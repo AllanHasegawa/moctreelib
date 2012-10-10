fabcc6a5bb6014d7381701a14cedf4048a648555
Allan Hasegawa <sirhase@gmail.com>
2012-10-10 00:49:35 -0300

    ** Working ClassicOctree::InsertCell * new Hello_World sample * redefined MOctree (AddCell -> InsertCell) * MOctree Interface now works with data as pointer only (to avoid copy)

--- sourcetree/moctree/classic/ClassicOctreeCell.h
+++ sourcetree/moctree/classic/ClassicOctreeCell.h
@@ -27,15 +27,19 @@
 #ifndef MOCTREE_CLASSICOCTREECELL_H_
 #define MOCTREE_CLASSICOCTREECELL_H_
 
+#include <stdint.h>
 #include <iostream>
+
+namespace moctree {
 
 template<class T>
 class ClassicOctreeCell {
  public:
-  ClassicOctreeCell(int x, int y, int z, T& data)
+  ClassicOctreeCell(uint32_t x, uint32_t y, uint32_t z, uint32_t size)
       : x_(x),
         y_(y),
         z_(z),
+        size_(size),
         data_(NULL),
         children_(NULL),
         father_(NULL) {
@@ -49,8 +53,16 @@ class ClassicOctreeCell {
     return children_[N];
   }
 
-  void CreateChildren() {
-    if (children_ == NULL) {
+  void CreateChildren(ClassicOctreeCell<T>* father) {
+    if (children_ == NULL && size_ != 1) {
+      const uint32_t size_father = father->size();
+      const uint32_t x_father = father->x();
+      const uint32_t y_father = father->y();
+      const uint32_t z_father = father->z();
+
+      const uint32_t size = size_father / 2;
+      const uint32_t mid_size = size / 2;
+
       // Lets create the children array
       children_ = new ClassicOctreeCell<T>*[8];
 
@@ -59,7 +71,10 @@ class ClassicOctreeCell {
       for (int z = 0; z < 2; z++) {
         for (int y = 0; y < 2; y++) {
           for (int x = 0; x < 2; x++) {
-            children_[counter] = new ClassicOctreeCell<T>(x, y, z);
+            children_[counter] = new ClassicOctreeCell<T>(
+                x_father + (x * mid_size), y_father + (y * mid_size),
+                z_father + (z * mid_size), size);
+            children_[counter]->set_father(father);
             counter++;
           }
         }
@@ -79,35 +94,91 @@ class ClassicOctreeCell {
     }
   }
 
-  void set_data(T& data) {
+  void InsertCell(const uint32_t x, const uint32_t y, const uint32_t z,
+                  T* data) {
+    if (size_ == 1) {
+      data_ = data;
+    } else {
+      this->CreateChildren(this);
+      this->children_[ConvertXYZToChildNumber(x, y, z)]->InsertCell(x, y, z,
+                                                                    data);
+    }
+  }
+
+  T* GetData(const uint32_t x, const uint32_t y, const uint32_t z) {
+
+    // if there is data BEFORE hitting the bottom of the tree
+    if (data_ != NULL) {
+      return data_;
+    }
+
+    // if is in the actual cell, but no data
+    if (size_ == 1) {
+      return NULL;
+    }
+
+    if (children_ == NULL) {
+      return NULL;
+    }
+
+    return this->children_[ConvertXYZToChildNumber(x, y, z)]->GetData(x, y, z);
+  }
+
+  void set_data(T* data) {
     data_ = data;
   }
 
-  void set_father(ClassicOctreeCell<T>& father) {
+  void set_father(ClassicOctreeCell<T>* father) {
     father_ = father;
   }
 
-  int x() {
+  uint32_t x() {
     return x_;
   }
 
-  int y() {
+  uint32_t y() {
     return y_;
   }
 
-  int z() {
+  uint32_t z() {
     return z_;
   }
 
-  T& data() {
+  uint32_t size() {
+    return size_;
+  }
+
+  T* data() {
     return data_;
   }
 
  private:
-  int x_;int y_;int z_;
+  uint32_t x_;
+  uint32_t y_;
+  uint32_t z_;
+  uint32_t size_;
   T* data_;
-  ClassicOctreeCell** children_;
-  ClassicOctreeCell* father_;
+  ClassicOctreeCell<T>** children_;
+  ClassicOctreeCell<T>* father_;
+
+  inline int ConvertXYZToChildNumber(const uint32_t x, const uint32_t y,
+                                     const uint32_t z) {
+    const uint32_t size = size_;
+    const uint32_t mid_size = size_ / 2;
+
+    const int bx = x >= mid_size;
+    const int by = y >= mid_size;
+    const int bz = z >= mid_size;
+
+    int child = 0;
+    child += bx;
+    child += (by << 1);
+    child += (bz << 2);
+
+    return child;
+  }
 };
+
+}  // namespace moctree
 
 #endif /* MOCTREE_CLASSICOCTREECELL_H_ */
